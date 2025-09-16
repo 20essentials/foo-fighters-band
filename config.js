a423e53f92997e0a0feeaadc01cf697bee4278d1
DominicDev <s0l0estudi4r@gmail.com>
2025-09-16 11:10:10 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'FOO FIGHTERS';
+export const nameSongs = [
+	"EVERLONG", /*1*/
+	"THE PRETENDER", /*2*/
+	"ALL MY LIFE", /*3*/
+	"MONKEY WRENCH", /*4*/
+	"WHEELS", /*5*/
+	"MY HERO", /*6*/
+	"BEST OF YOU", /*7*/
+	"LEARN TO FLY", /*8*/
+	"GENERATOR", /*9*/
+	"AURORA", /*10*/
+	"NEXT YEAR", /*11*/
+	"WALK", /*12*/
+	"THESE DAYS", /*13*/
+	"BRIDGE BURNING", /*14*/
+	"BREAKOUT", /*15*/
+	"GOOD GRIEF", /*16*/
+	"EXHAUSTED", /*17*/
+	"X-STATIC", /*18*/
+	"COME BACK", /*19*/
+	"TIRED OF YOU", /*20*/
+]
